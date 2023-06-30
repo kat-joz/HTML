42304aa590491554f23ddac297bbec12d90da922
Your Name <you@example.com>
2023-06-30 14:41:52 +0100

    iterations exercise

---
+++ Java/iteration.js
@@ -0,0 +1,22 @@
+let a = 100
+while (a<200){
+    a++;
+    console.log("a" + "=" + a)
+}
+
+let b = 100
+while (b<=200){
+
+if (b%2==0){
+    console.log("-")
+}
+else {
+    console.log("*")
+}
+b++
+}
+
+for (let c = 1; c < 10; c++){
+    for (let d = 1; d <=10; d++){
+    console.log(d)}
+}
